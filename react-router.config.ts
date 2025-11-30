cec3670c248aadc0a57187dedfacbf006c701fc6
Yuki Nishina <project_flower@outlook.com>
2025-11-30 17:59:37 +0900

    本番環境向け設定を追加

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
+  basename: import.meta.env.PROD ? "/<repo>/" : "/",
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/<repo>/" : undefined,
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+}));
